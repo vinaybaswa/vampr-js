0dc19aca9280a1fd3328ed5d8f487fba27dec7dc
Vinay Baswa <vinaybaswa@gmail.com>
2020-09-05 16:00:43 +0000

    vampire tree

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numOFVamps = 0;
+    let curVamp = this;
+    while (curVamp.creator) {
+      curVamp = curVamp.creator;
+      numOFVamps++;
+    }
+    return numOFVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,9 +43,39 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    // if (this === vampire) {
+    //   return vampire;
+    // } else if (this.creator === vampire.creator) {
+    //   return this.creator;
+    // } else if (this.creator === vampire) {
+    //   return vampire;
+    // } else if (this.offspring = vampire) {
+    //   return this;
+    // }
   }
 }
+
+// const original = new Vampire("Original", 1900);
+
+// //Original
+// const ansel = new Vampire("Ansel", 1920);
+// const bart = new Vampire("Bart", 1925);
+// original.addOffspring(ansel);
+// original.addOffspring(bart);
+
+// //Ansel
+// const sarah = new Vampire("Sarah", 1940);
+// const elgort = new Vampire("Elgort", 1945);
+// ansel.addOffspring(sarah);
+// ansel.addOffspring(elgort);
+
+// //Elgort
+// const andrew = new Vampire("Andrew", 1965);
+// elgort.addOffspring(andrew);
+
+// console.log(andrew.closestCommonAncestor(sarah));
+// console.log(original.isMoreSeniorThan(ansel));
+// console.log(ansel)
 
 module.exports = Vampire;
 
